0e046c892c1ba63cdb2fa6c55c16dc0a38490991
silonest <silonest@icloud.com>
2017-05-11 14:55:01 +0800

    可以给软件绑定角色了

--- hzmanager/src/components/software/controller.vue
+++ hzmanager/src/components/software/controller.vue
@@ -14,12 +14,16 @@
       <div class="ui button" @click="showNewSwMod()">新增软件</div>
     </div>
   </div>
+  <!-- <h5 class="ui center aligned icon header" v-if="selectedSoftware != null && selectedSoftware.note != null"><img class="ui centered medium image" :src="'/resource/static/' + selectedSoftware.softwareImg"> {{selectedSoftware.note.softwareName}} </h5> -->
+  <!-- <h5 class="ui header"><img :src="'/resource/static/' + selectedSoftware.softwareImg" class="ui circular image"> {{selectedSoftware.note.softwareName}} </h5> -->
   <!--点击软件后显示的操作区，如果不点击软件，则给出提示消息。-->
-
   <div class="ui basic segment" style="padding:0px;" v-show="selectedSoftware != null && selectedSoftware.softwareId != null && selectedSoftware.softwareId != ''">
     <div id="softwareControllerTab" class="ui pointing secondary menu">
       <a class="item active" data-tab="version">最近版本</a>
       <a class="item" data-tab="role">关联角色</a>
+      <div class="right item" style="padding:8px 5px;" v-if="selectedSoftware != null && selectedSoftware.note != null">
+        <div class="ui label">{{selectedSoftware.note.softwareName}}</div>
+      </div>
     </div>
     <!--分支的快捷操作-->
     <div class="active ui bottom attached tab segment" data-tab="version" style="height:285px;padding:10px;margin-bottom:0px;">
@@ -28,11 +32,11 @@
       </div>
       <div class="ui grid" style="height:50px;">
         <div class="left floated left aligned ten wide column" style="padding:18px 0px 15px 20px;">
-          <h4>共 <div class="ui mini circular label">{{branch.count}}</div> 个版本</h4>
+          <h5>共 <div class="ui mini circular label">{{branch.count}}</div> 个版本</h5>
         </div>
         <div class="right floated right aligned six wide column" style="padding:18px 15px 15px 5px;">
           <a href="javascript:return" @click="showBranchsBelongSw(selectedSoftware.softwareId)" data-content="点击查看全部" data-variation="mini inverted" data-position="right center"><i class="list layout blue outline icon"></i></a>
-          <a href="javascript:return" @click="showNewVerMod()" data-content="点击添加新版本" data-variation="mini inverted" data-position="right center"><i class="add green outline icon"></i></a>
+          <a href="javascript:return" @click="showNewVerMod()" data-content="点击添加新版本" data-variation="mini inverted" data-position="right center"><i class="add blue outline icon"></i></a>
         </div>
       </div>
       <div class="ui divider" style="margin: 14px 0px 2px 0px;"></div>
@@ -51,30 +55,33 @@
       </div>
     </div>
     <!--关联角色的快捷操作-->
-    <div class="ui bottom attached tab segment" data-tab="role" style="height:305px;padding:10px;margin-bottom:0px;">
-      <div class="ui grid" style="height:50px;">
-        <div class="left floated left aligned ten wide column" style="padding:18px 0px 15px 20px;">
-          <h4>软件共 <a class="ui mini circular label" @click="showBranchsBelongSw(selectedSoftware.softwareId)">{{branch.count}}</a> 个角色</h4>
-        </div>
-        <!-- <div class="right floated right aligned six wide column" style="padding:12px 15px 10px 5px;">
-          <button class="ui mini icon button" @click="showNewVerMod()" style="margin:0px;"><i class="add icon"></i></button>
-        </div> -->
-      </div>
-      <div class="ui divider" style="margin: 14px 0px 2px 0px;"></div>
-      <div class="ui very relaxed divided list" style="margin-top:2px;">
-
-        <div class="item" v-for="item in roles" style="padding:6px 0px;">
-          <!-- <div class="right floated content">
-            <button class="ui mini icon button" style="margin:0px;"><i class="download icon"></i></button>
-          </div> -->
-          <img class="ui middle aligned avatar image" :src="'/resource/static/' + selectedSoftware.softwareImg">
-          <div class="content">
-            <a class="header">{{item.roleName}}</a>
-            <div class="description">{{item.roleIntroduction | substr(13)}}</div>
-          </div>
-        </div>
-
-      </div>
+    <div class="ui bottom attached tab segment" data-tab="role" style="height:400px;padding:0;margin-bottom:0px;">
+      <table class="ui small compact single line table" style="margin:0;">
+        <thead>
+          <tr>
+            <th>角色名</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in roles">
+            <td>{{item.roleName}}</td>
+            <td class="collapsing">
+              <div class="ui fitted slider checkbox">
+                <input type="checkbox" v-model="item.selected" @click="changeCheckBox(item)"><label></label>
+              </div>
+            </td>
+          </tr>
+          <!-- <tr v-for="item in roles">
+            <td></td>
+            <td class="collapsing">
+              <div class="ui fitted slider checkbox">
+                <input type="checkbox" @click="changeCheckBox(item)" v-model="item.selected"> <label></label>
+              </div>
+            </td>
+          </tr> -->
+        </tbody>
+      </table>
     </div>
   </div>
   <div class="ui icon ignored yellow message" v-show="selectedSoftware == null || selectedSoftware.softwareId == null || selectedSoftware.softwareId == ''">
@@ -104,10 +111,23 @@ export default {
     }
   },
   methods: {
+    objArrayHandler(arry) {
+      if (arry != null) {
+        for (let i = 0; i < arry.length; i++) {
+          let obj = arry[i];
+          if (obj.softwareId != null) {
+            obj['selected'] = true;
+          } else {
+            obj['selected'] = false;
+          }
+          arry[i] = obj;
+        }
+        return arry;
+      }
+    },
     fillVersions() {
       axios.get('/resource/dynamic/software/' + this.selectedSoftware.softwareId + '/latest/version')
         .then(response => {
-          console.log(response);
           let branch = new Object();
           branch.count = response.data.content.totalCount;
           branch.item = response.data.content.branch;
@@ -119,7 +139,18 @@ export default {
     fillRoles() {
       axios.get('/resource/dynamic/software/' + this.selectedSoftware.softwareId + '/latest/role')
         .then(response => {
-          this.roles = response.data.content;
+          this.roles = this.objArrayHandler(response.data.content);
+        }).catch(function(error) {
+          alert(error);
+        });
+    },
+    changeCheckBox(role) {
+      let bindType = role.selected ? "bind" : "unbind";
+      axios.put('/resource/dynamic/software/' + this.selectedSoftware.softwareId + '/role/' + role.roleId + '/' + bindType)
+        .then(response => {
+          if (response.data.status == 'SUCCESS') {
+            role.selected ? this.toast.success('绑定成功') : this.toast.success('解除绑定');
+          }
         }).catch(function(error) {
           alert(error);
         });
